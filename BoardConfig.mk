e85f4357f7250c284c0089e25c7c51fa3f66c0e3
together08 <together08@yeah.net>
2022-05-08 08:24:54 +0800

    cupid: Remove duplicate rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,7 +101,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8450
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 cgroup.memory=nokmem,nosocket console=ttyMSM0,115200n8 loop.max_part=7 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=0 pcie_ports=compat iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_APPEND_DTB := false
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
