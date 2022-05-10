6cd5d4c2fbd4417124a49d272aa6f7fd1353a37d
together08 <together08@yeah.net>
2022-05-10 08:13:16 +0800

    cupid: Update drm to 1.4 and also build widevine

--- device.mk
+++ device.mk
@@ -174,8 +174,10 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4-service.widevine \
+    liboemcrypto
 
 # DTB
 PRODUCT_COPY_FILES += \
